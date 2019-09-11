eafa984db5e714171713060585226867628f2b07
Ryan Shan <hanshanr@tw.ibm.com>
2019-09-12 02:48:40 +0800

    update info and img

--- resources/js/gal.script.js
+++ resources/js/gal.script.js
@@ -1,21 +1,21 @@
 function openTab(part) {
-    var tabs = document.getElementsByClassName('gallery-tab');
-    
-    for(var i = 0; i<tabs.length; i++){
-        tabs[i].style.display = "none";   
-    }
-    document.getElementById('gallery-main').style.display = "none";
-    document.getElementById("part" + part).style.display = "block";
+  var tabs = document.getElementsByClassName('gallery-tab');
+
+  for (var i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = "none";
+  }
+  document.getElementById('gallery-main').style.display = "none";
+  document.getElementById("part" + part).style.display = "block";
 }
 
 function closeTab() {
-    var tabs = document.getElementsByClassName('gallery-tab');
-    
-    for(var i = 0; i<tabs.length; i++){
-        tabs[i].style.display = "none";   
-    }
-    
-    document.getElementById('gallery-main').style.display = "block";
+  var tabs = document.getElementsByClassName('gallery-tab');
+
+  for (var i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = "none";
+  }
+
+  document.getElementById('gallery-main').style.display = "block";
 }
 
 function openModal() {
@@ -35,7 +35,7 @@ function closeModal() {
 var slideIndex = 1;
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  showSlides(slideIndex = n);
 }
 
 function plusSlides(n) {
@@ -52,46 +52,52 @@ function showSlides(n) {
   var dots = document.getElementsByClassName("demo");
   var show = document.getElementById("slideShow");
 
-  if (n > 86) {slideIndex = 1}
-  else if (n < 1) {slideIndex = 86}
-  else {slideIndex = n}
+  if (n > 90) { slideIndex = 1 }
+  else if (n < 1) { slideIndex = 90 }
+  else { slideIndex = n }
 
-  
-    
-  
+
+
+
   show.src = getPhotoSRC(slideIndex);
   slides[0].style.display = "block";
-  
+
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-      dots[i].src = "/resources/img/gallery/thumbnail/"+((i+slideIndex-4+86)%86+1)+".jpg";
+    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].src = "/resources/img/gallery/thumbnail/" + ((i + slideIndex - 4 + 86) % 86 + 1) + ".jpg";
   }
-    
-  dots[slideIndex-1].className += " active";
+
+  dots[slideIndex - 1].className += " active";
 }
 
-function getPhotoSRC(slideIndex){
-    let photosrc;
-  
-    if(slideIndex>0&&slideIndex<=10){photosrc = "/resources/img/gallery/A/"+(slideIndex)+".jpg";}
-    else if(slideIndex>10&&slideIndex<=18){photosrc = "/resources/img/gallery/B/"+(slideIndex)+".jpg";}
-    else if(slideIndex>18&&slideIndex<=36||slideIndex==83||slideIndex==84){photosrc = "/resources/img/gallery/C/"+(slideIndex)+".jpg";}
-    else if(slideIndex>36&&slideIndex<=45){photosrc = "/resources/img/gallery/D/"+(slideIndex)+".jpg";}
-    else if(slideIndex>45&&slideIndex<=62){photosrc = "/resources/img/gallery/E/"+(slideIndex)+".jpg";}
-    else if(slideIndex>62&&slideIndex<=72||slideIndex==85||slideIndex==86){photosrc = "/resources/img/gallery/F/"+(slideIndex)+".jpg";}
-    else if(slideIndex>72&&slideIndex<=82){photosrc = "/resources/img/gallery/G/"+(slideIndex)+".jpg";}
-    else {photosrc = "/resources/img/gallery/thumbnail/"+(slideIndex)+".jpg";}
-    
-    return photosrc;
+function getPhotoSRC(slideIndex) {
+  const photosrc = "/resources/img/gallery/";
+
+  if (slideIndex > 0 && slideIndex <= 10) { photosrc = `${photosrc}A/${slideIndex}.jpg`; }
+  else if (slideIndex > 10 && slideIndex <= 18) { photosrc = `${photosrc}B/${slideIndex}.jpg`; }
+  else if (slideIndex > 18 && slideIndex <= 36 || slideIndex === 83 || slideIndex === 84 || slideIndex === 90) { photosrc = `${photosrc}C/${slideIndex}.jpg`; }
+  else if (slideIndex > 36 && slideIndex <= 45) { photosrc = `${photosrc}D/${slideIndex}.jpg`; }
+  else if (slideIndex > 45 && slideIndex <= 62 || slideIndex === 87 || slideIndex === 88) { photosrc = `${photosrc}E/${slideIndex}.jpg`; }
+  else if (slideIndex > 62 && slideIndex <= 72 || slideIndex === 85 || slideIndex === 86 || slideIndex === 89) { photosrc = `${photosrc}F/${slideIndex}.jpg`; }
+  else if (slideIndex > 72 && slideIndex <= 82) { photosrc = `${photosrc}G/${slideIndex}.jpg`; }
+  else { photosrc = `${photosrc}thumbnail/${slideIndex}.jpg`; }
+
+  return photosrc;
 }
 
 /*window.onload = function() {
 	setTimeout(function() {
 		// preload image
         let i;
-        
+
         for(i=1; i<87; i++){
 		  new Image().src = getPhotoSRC(i);
         }
 	}, 5000);
 };*/
+
+document.querySelector("modal").addEventListener('keydown', function(e){
+  if(e.keyCode === 27) {
+    closeModal();
+  }
+}, false);
